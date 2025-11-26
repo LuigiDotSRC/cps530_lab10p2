ae4973e6ba8a0d2b9fb5fc4897b107c8291a6605
LuigiDotSRC <luigi.terashima@gmail.com>
2025-11-26 14:01:56 -0500

    add snowflakes and text

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,85 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
+interface Flake {
+  x: number;
+  y: number;
+  radius: number;
+  speed: number;
+}
+
 export default function Home() {
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    
+    canvas.width = width;
+    canvas.height = height;
+
+    const FLAKES = 200;
+    const flakes: Flake[] = [];
+
+    for (let i = 0; i < FLAKES; i++) {
+      flakes.push({
+        x: Math.random() * width,
+        y: Math.random() * height,
+        radius: 1 + Math.random() * 3,
+        speed: 0.5 + Math.random() * 1.2,
+      });
+    }
+   
+    const update = () => {
+      for (const f of flakes) {
+        f.y += f.speed;
+
+        if (f.y > height) {
+          f.y = -5;
+          f.x = Math.random() * width;
+        }
+      }
+    };
+
+    const draw = () => {
+      ctx.clearRect(0, 0, width, height);
+
+      ctx.fillStyle = "white";
+      ctx.beginPath();
+
+      for (const f of flakes) {
+        ctx.moveTo(f.x + f.radius, f.y);
+        ctx.arc(f.x, f.y, f.radius, 0, Math.PI * 2);
+      }
+
+      ctx.fill();
+    };
+
+    let id: number;
+    const loop = () => {
+      update();
+      draw();
+      id = requestAnimationFrame(loop);
+    };
+
+    loop();
+
+    return () => {
+      cancelAnimationFrame(id);
+    };
+  }, []);
+
   return (
     // <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-    <main> 
-      <p>Hello world</p>
+    <main className="relative flex justify-center items-center w-full h-screen"> 
+      <canvas ref={canvasRef} className="absolute inset-0 w-full h-full"></canvas>
+      <p className="absolute text-[#edf9ff] font-bold text-9xl text-shadow-lg">End of Fall 2025 Semester</p>
     </main>
     //</div>
   );
